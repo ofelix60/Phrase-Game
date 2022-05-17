6545c6702c7709195ef23d18363b404379f755b7
Oscar Felix <oscar@Oscars-MacBook-Pro.local>
2022-05-16 19:19:31 -0700

    added simple keyboard interactivity

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const debugPhrase = 'hellow World';
+const debugPhrase = 'Hello World';
+const phraseBox = document.querySelector('#phrase ul');
+const phraseItems = document.querySelectorAll('li');
 
 // display the phrase in blank boxes on the screen
 let htmlString = '';
 for (let letter of debugPhrase) {
-	if (letter !== '') {
+	if (letter !== ' ') {
 		htmlString += `<li class="letter">${letter}</li>`;
 	} else {
 		htmlString += `<li class="space"></li>`;
 	}
 }
 
-const phraseBox = document.querySelector('#phrase ul');
 phraseBox.innerHTML = htmlString;
+
+document.addEventListener('keypress', e => {
+	console.log(e.key);
+});
