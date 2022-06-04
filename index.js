8bcb933a6a655175a42b129529830e5ff9a899a3
Oscar Felix <ofelix60@gmail.com>
2022-06-04 12:54:22 -0700

    adding 'loosing a life' functionality if user guesses wrong. Added hearts on screen that disappear every time the user guesses incorrectly.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const debugPhrase = 'Hello World';
 const phraseBox = document.querySelector('#phrase ul');
 const phraseItems = document.getElementsByTagName('li');
 const letterBtns = document.getElementsByTagName('button');
+const lives = document.getElementsByTagName('span');
 
 // a function that displays any string/phrase in blank boxes on the screen
 function phraseDisplay(phrase) {
@@ -24,16 +25,33 @@ function winChecker() {
 	}
 }
 
+function takeDamage() {
+	for (let life of lives) {
+		if (life.classList.contains('hide')) {
+			continue;
+		} else {
+			life.classList.add('hide');
+			break;
+		}
+	}
+}
+
 // Listens for on-screen button click and displays all instances of letter in phrase
 document.getElementById('qwerty').addEventListener('click', e => {
+	let damage = true;
 	if (e.target.tagName === 'BUTTON') {
 		for (let box of phraseItems) {
-			if (e.target.innerText == box.innerText) {
+			if (e.target.innerText === box.innerText) {
 				box.classList.add('show');
 				winChecker();
+				damage = false;
 			}
 		}
 	}
+	if (damage === true) {
+		takeDamage();
+	}
+	damage = true;
 	e.target.disabled = true;
 });
 
